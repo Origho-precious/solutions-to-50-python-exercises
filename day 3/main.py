b3e9ae4a92c0c6a8176f6c17616ab73a9ef1f5a8
Origho-precious <Origho9@gmail.com>
2022-09-29 04:28:21 +0100

    feat: adding alt solution to question 13

--- day 3/main.py
+++ day 3/main.py
@@ -56,7 +56,7 @@
 # QUESTION 13: Write a program that accepts a sentence and calculate the number of letters and digits.
 
 # Suppose the following input is supplied to the program:
-# hello world! 123
+# 
 
 # Then, the output should be:
 # LETTERS 10
@@ -74,3 +74,19 @@
 
 # print(f'LETTERS: {len(letters)}')
 # print(f'DIGITS: {len(digits)}')
+
+
+# ----------OR----------
+
+# input_value = input('Enter: ')
+
+# letters, digits = 0, 0
+
+# for i in input_value:
+#     if i.isalpha():
+#         letters += 1
+#     elif i.isnumeric():
+#         digits += 1
+
+# print(f'LETTERS: {letters}')
+# print(f'DIGITS: {digits}')
