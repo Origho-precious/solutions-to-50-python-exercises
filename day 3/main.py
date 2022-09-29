5c9ef0c69e65764a577859ad117167a3b60cffaa
Origho-precious <Origho9@gmail.com>
2022-09-29 04:24:32 +0100

    feat: adding solutions to day 3 challenges

--- day 3/main.py
+++ day 3/main.py
@@ -9,6 +9,15 @@
 # Hints:
 # In case of input data being supplied to the question, it should be assumed to be a console input.We use set container to remove duplicated data automatically and then use sorted() to sort the data.
 
+# input_value = input('Enter a sentence: ').split(' ')
+
+# SENTENCE = ''
+
+# for word in sorted(set(input_value)):
+#     SENTENCE += f'{word} '
+
+# print(SENTENCE)
+
 
 # QUESTION 11: Write a program which accepts a sequence of comma separated 4 digit binary numbers as its input and then check whether they are divisible by 5 or not. The numbers that are divisible by 5 are to be printed in a comma separated sequence.
 
@@ -17,26 +26,51 @@
 
 # Then the output should be:
 # 1010
-# Notes: Assume the data is input by console.
 
 # Hints:
 # In case of input data being supplied to the question, it should be assumed to be a console input.
+
+# value = input('Enter comma seperated 4 digit binary numbers: ').split(',')
+
+# value = [int(num) for num in value]
+# print(value)
+
+# value = [num for num in value if (len(str(num)) == 4) and (num % 5 == 0)]
+
+# print(','.join(map(str, value)))
 
 
 # QUESTION 12: Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the number is an even number.The numbers obtained should be printed in a comma-separated sequence on a single line.
 
-# Hints:
-# In case of input data being supplied to the question, it should be assumed to be a console input.
+# nums = []
+
+# str_nums = [str(num) for num in range(1000, 3001)]
+
+# for num in str_nums:
+#     if (int(num[0]) % 2 == 0) and (int(num[1]) % 2 == 0) and (int(num[2]) % 2 == 0) and (int(num[3]) % 2 == 0):
+#         nums.append(num)
+
+# print(','.join(nums))
 
 
 # QUESTION 13: Write a program that accepts a sentence and calculate the number of letters and digits.
 
 # Suppose the following input is supplied to the program:
-
 # hello world! 123
-# Then, the output should be:
 
+# Then, the output should be:
 # LETTERS 10
 # DIGITS 3
+
 # Hints:
 # In case of input data being supplied to the question, it should be assumed to be a console input.
+
+# import re
+
+# input_value = input('Enter: ')
+
+# letters = re.findall(r'[a-zA-Z]', input_value)
+# digits = re.findall(r'\d', input_value)
+
+# print(f'LETTERS: {len(letters)}')
+# print(f'DIGITS: {len(digits)}')
