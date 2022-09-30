bddcc15515fa9e55a3fd793584cb694c35199672
Origho-precious <Origho9@gmail.com>
2022-09-30 03:45:30 +0100

    feat: completing day 4 challenge

--- day 4/main.py
+++ day 4/main.py
@@ -11,6 +11,23 @@
 # Hints:
 # In case of input data being supplied to the question, it should be assumed to be a console input.
 
+from functools import reduce
+
+
+input_value = list(input('Enter a sentence: '))
+
+UPPERCASE = 0
+LOWERCASE = 0
+
+for letter in input_value:
+    if letter.isupper():
+        UPPERCASE += 1
+    elif letter.islower():
+        LOWERCASE += 1
+
+print(f'UPPER CASE {UPPERCASE}')
+print(f'LOWER CASE {LOWERCASE}')
+
 
 # Question 15:
 # Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
@@ -19,7 +36,20 @@
 
 # 9
 # Then, the output should be:
-
 # 11106
+
 # Hints:
 # In case of input data being supplied to the question, it should be assumed to be a console input.
+
+
+number = input('Enter a number: ')
+
+total = int(number) + int(number * 2) + int(number * 3) + int(number * 4)
+
+print(total)
+
+
+# ---------OR---------------
+
+x = input('Enter a number: ')
+reduce(lambda x, y: int(x) + int(y), [x*i for i in range(1, 5)])
