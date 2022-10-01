9e7823ef595e3ed92af061688fe5de3bb3c14f1f
Origho-precious <Origho9@gmail.com>
2022-10-01 01:11:43 +0100

    feat: adding question 12

---
+++ day 12/main.py
@@ -0,0 +1,19 @@
+# Question 44:
+# Write a program which can map() to make a list whose elements are square of numbers between 1 and 20 (both included).
+
+# Hints:
+# Use map() to generate a list. Use lambda to define anonymous functions.
+
+
+# Question 45:
+# Define a class named American which has a static method called printNationality.
+
+# Hints:
+# Use @staticmethod decorator to define class static method.There are also two more methods.To know more, go to this link.
+
+
+# Question 46:
+# Define a class named American and its subclass NewYorker.
+
+# Hints:
+# Use class Subclass(ParentClass) to define a subclass.*
