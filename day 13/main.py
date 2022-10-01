7f988f4ea343db2379833f078f46ff46e4b242cf
Origho-precious <Origho9@gmail.com>
2022-10-01 01:12:04 +0100

    feat: adding question 13

---
+++ day 13/main.py
@@ -0,0 +1,26 @@
+# Question 47;
+# Define a class named Circle which can be constructed by a radius. The Circle class has a method which can compute the area.
+
+# Hints
+# Use def methodName(self) to define a method.
+
+
+# Question 48:
+# Define a class named Rectangle which can be constructed by a length and width. The Rectangle class has a method which can compute the area.
+
+# Hints
+# Use def methodName(self) to define a method.
+
+
+# Question 49:
+# Define a class named Shape and its subclass Square. The Square class has an init function which takes a length as argument. Both classes have a area function which can print the area of the shape where Shape's area is 0 by default.
+
+# Hints:
+# To override a method in super class, we can define a method with the same name in the super class.
+
+
+# Question 50:
+# Please raise a RuntimeError exception.
+
+# Hints:
+# UUse raise () to raise an exception.
