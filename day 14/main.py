f75079c0f867ed50e2edb78a240a9e693f3b8fee
Origho-precious <Origho9@gmail.com>
2022-10-01 01:12:15 +0100

    feat: adding question 14

---
+++ day 14/main.py
@@ -0,0 +1,27 @@
+# Question 51:
+# Write a function to compute 5/0 and use try / except to catch the exceptions.
+
+# Hints:
+# Use try / except to catch exceptions.
+
+
+# Question 52:
+# Define a custom exception class which takes a string message as attribute.
+
+# Hints:
+# To define a custom exception, we need to define a class inherited from Exception.
+
+
+# Question 53:
+# Assuming that we have some email addresses in the "username@companyname.com" format, please write program to print the user name of a given email address. Both user names and company names are composed of letters only.
+
+# Example: If the following email address is given as input to the program:
+# john@google.com
+
+# Then, the output of the program should be:
+# john
+
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+
+# Hints
+# Use \w to match letters.
