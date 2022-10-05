a28684cb4f700e42acb0b0771ae3c0e8ef1e361d
Origho-precious <Origho9@gmail.com>
2022-10-05 19:07:15 +0100

    feat: adding solutions to Question 44 - 46

--- day 12/main.py
+++ day 12/main.py
@@ -4,6 +4,9 @@
 # Hints:
 # Use map() to generate a list. Use lambda to define anonymous functions.
 
+square_nums = map(lambda num: num**2, range(1, 21))
+
+print(list(square_nums))
 
 # Question 45:
 # Define a class named American which has a static method called printNationality.
@@ -12,8 +15,23 @@
 # Use @staticmethod decorator to define class static method.There are also two more methods.To know more, go to this link.
 
 
+class American:
+    @staticmethod
+    def print_nationality():
+        print('American!')
+
+
+American.print_nationality()
+
 # Question 46:
 # Define a class named American and its subclass NewYorker.
 
 # Hints:
 # Use class Subclass(ParentClass) to define a subclass.*
+
+
+class NewYorker(American):
+
+    @staticmethod
+    def occupation():
+        print('Software Engineering')
