1e002bf8c0c34c641cd41cc39c60039eac1fa120
Origho-precious <Origho9@gmail.com>
2022-10-05 17:53:29 +0100

    feat: adding solutions to Question 38 & 39

--- day 11/main.py
+++ day 11/main.py
@@ -4,6 +4,9 @@
 # Hints:
 # Use[n1:n2] notation to get a slice from a tuple.
 
+my_tup = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+print(' '.join(map(lambda num: str(num), my_tup[0:5])))
 
 # Question 39:
 # Write a program to generate and print another tuple whose values are even numbers in the given tuple(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).
@@ -11,6 +14,13 @@
 # Hints:
 # Use "for" to iterate the tuple. Use tuple() to generate a tuple from a list.
 
+even_nums = []
+
+for num in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    if num % 2 == 0:
+        even_nums.append(num)
+
+print(tuple(even_nums))
 
 # Question 40:
 # Write a program which accepts a string as input to print "Yes" if the string is "yes" or "YES" or "Yes", otherwise print "No".
